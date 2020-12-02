9e4518ff12b99dea8962ed26b61faf70d5b2c07e
Daniel Arenhart <arenhart.daniel@gmail.com>
2020-12-02 16:03:24 -0300

    Update initial tasks

--- src/data.js
+++ src/data.js
@@ -1,66 +1,52 @@
-import colors from './colors';
-
-const frontend = {
+const backlog = {
   id: '1',
-  name: 'Frontend',
-  colors: {
-    soft: colors.Y50,
-    hard: colors.N400A,
-  },
+  name: 'Backlog',
 };
 
-const backend = {
+const doing = {
   id: '2',
-  name: 'Backend',
-  colors: {
-    soft: colors.G50,
-    hard: colors.N400A,
-  },
+  name: 'Doing',
 };
 
-const ux = {
+const done = {
   id: '3',
-  name: 'UX',
-  colors: {
-    soft: colors.B50,
-    hard: colors.N400A,
-  },
+  name: 'Done',
 };
 
-export const badges = [frontend, backend, ux];
+export const badges = [backlog, doing, done];
 
 export const tasks = [
   {
     id: '2',
     content:
       'Sucking at something is the first step towards being sorta good at something.',
-    badge: frontend,
+    badge: backlog,
   },
   {
     id: '3',
     content: "You got to focus on what's real, man",
-    badge: frontend,
+    badge: backlog,
   },
   {
     id: '4',
     content: 'Is that where creativity comes from? From sad biz?',
-    badge: backend,
+    badge: doing,
   },
   {
     id: '5',
     content: 'Homies help homies. Always',
-    badge: ux,
+    badge: done,
   },
   {
     id: '8',
     content:
       "People make mistakes. It's all a part of growing up and you never really stop growing",
-    badge: ux,
+    badge: done,
   },
   {
     id: '9',
     content: "Don't you always call sweatpants 'give up on life pants,' Jake?",
-    badge: ux,
+    badge: done,
   },
 ];
 
